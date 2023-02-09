7cc0f40480af6cba3c4df44b79a542161eadc59c
Osman Saral <osman.saral@gmail.com>
2023-02-09 21:35:11 +0300

    Expose HttpMethod

--- source/index.ts
+++ source/index.ts
@@ -1,23 +1,27 @@
 /*! MIT License © Sindre Sorhus */
 
-import {Ky} from './core/Ky.js';
-import {requestMethods, stop} from './core/constants.js';
-import type {KyInstance} from './types/ky.js';
-import type {Input, Options} from './types/options.js';
-import {validateAndMerge} from './utils/merge.js';
-import {Mutable} from './utils/types.js';
+import { Ky } from "./core/Ky.js";
+import { requestMethods, stop } from "./core/constants.js";
+import type { KyInstance } from "./types/ky.js";
+import type { Input, Options } from "./types/options.js";
+import { validateAndMerge } from "./utils/merge.js";
+import { Mutable } from "./utils/types.js";
 
 const createInstance = (defaults?: Partial<Options>): KyInstance => {
 	// eslint-disable-next-line @typescript-eslint/promise-function-async
-	const ky: Partial<Mutable<KyInstance>> = (input: Input, options?: Options) => Ky.create(input, validateAndMerge(defaults, options));
+	const ky: Partial<Mutable<KyInstance>> = (input: Input, options?: Options) =>
+		Ky.create(input, validateAndMerge(defaults, options));
 
 	for (const method of requestMethods) {
 		// eslint-disable-next-line @typescript-eslint/promise-function-async
-		ky[method] = (input: Input, options?: Options) => Ky.create(input, validateAndMerge(defaults, options, {method}));
+		ky[method] = (input: Input, options?: Options) =>
+			Ky.create(input, validateAndMerge(defaults, options, { method }));
 	}
 
-	ky.create = (newDefaults?: Partial<Options>) => createInstance(validateAndMerge(newDefaults));
-	ky.extend = (newDefaults?: Partial<Options>) => createInstance(validateAndMerge(defaults, newDefaults));
+	ky.create = (newDefaults?: Partial<Options>) =>
+		createInstance(validateAndMerge(newDefaults));
+	ky.extend = (newDefaults?: Partial<Options>) =>
+		createInstance(validateAndMerge(defaults, newDefaults));
 	ky.stop = stop;
 
 	return ky as KyInstance;
@@ -33,7 +37,8 @@ export type {
 	RetryOptions,
 	SearchParamsOption,
 	DownloadProgress,
-} from './types/options.js';
+	HttpMethod,
+} from "./types/options.js";
 
 export type {
 	Hooks,
@@ -42,9 +47,9 @@ export type {
 	BeforeRetryState,
 	BeforeErrorHook,
 	AfterResponseHook,
-} from './types/hooks.js';
+} from "./types/hooks.js";
 
-export type {ResponsePromise} from './types/ResponsePromise.js';
-export type {KyResponse} from './types/response.js';
-export {HTTPError} from './errors/HTTPError.js';
-export {TimeoutError} from './errors/TimeoutError.js';
+export type { ResponsePromise } from "./types/ResponsePromise.js";
+export type { KyResponse } from "./types/response.js";
+export { HTTPError } from "./errors/HTTPError.js";
+export { TimeoutError } from "./errors/TimeoutError.js";
