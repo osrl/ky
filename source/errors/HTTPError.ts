bf1cbebe73b0cf8ac7d1f53eee1f5ff93c376074
Osman Saral <osman.saral@gmail.com>
2023-02-14 15:27:41 +0300

    fix: error body consuming

--- source/errors/HTTPError.ts
+++ source/errors/HTTPError.ts
@@ -1,4 +1,4 @@
-import type {NormalizedOptions} from '../types/options.js';
+import type { NormalizedOptions } from "../types/options.js";
 
 // eslint-lint-disable-next-line @typescript-eslint/naming-convention
 export class HTTPError extends Error {
@@ -6,21 +6,26 @@ export class HTTPError extends Error {
 	public request: Request;
 	public options: NormalizedOptions;
 
-	constructor(response: Response, request: Request, options: NormalizedOptions) {
-		const code = (response.status || response.status === 0) ? response.status : '';
-		const title = response.statusText || '';
+	constructor(
+		response: Response,
+		request: Request,
+		options: NormalizedOptions
+	) {
+		const code =
+			response.status || response.status === 0 ? response.status : "";
+		const title = response.statusText || "";
 		const status = `${code} ${title}`.trim();
-		const reason = status ? `status code ${status}` : 'an unknown error';
+		const reason = status ? `status code ${status}` : "an unknown error";
 
 		super(`Request failed with ${reason}`);
 
-		this.name = 'HTTPError';
+		this.name = "HTTPError";
 		this.response = response;
 		this.request = request;
 		this.options = options;
 	}
 
 	async json<F = any>(): Promise<F> {
-		return this.response.json() as F;
+		return this.response.clone().json() as F;
 	}
 }
